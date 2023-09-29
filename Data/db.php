0181b91ca1f948c0a9c603511887de15478d98f7
StefanoMela <stefano.mela25@gmail.com>
2023-09-29 16:25:55 +0200

    feat: little scaffolding

---
+++ Data/db.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+$genres = [
+
+    'Action',
+    'Thriller',
+    'Sci-Fi',
+    'Drama',
+    'Biopic',
+    'Fantasy',
+    'Horror',
+    'Romantic',
+]
+
+?>
--- index.php
+++ index.php
@@ -1,43 +1,7 @@
 <?php 
 
-require_once __DIR__ . '/Models/Movies.php';
-require_once __DIR__ . '/Models/Genre.php';
+include 'server.php';
 
-
-$new_genre = new Genre(
-
-    'Action',
-    'Horror',
-
-);
-$new_genre_2 = new Genre(
-
-    'Sci-Fi',
-    'Drama',
-
-);
-
-
-$new_movie = new Movie(
-    'Iron Man',
-    $new_genre,
-    'US / CAN',
-    '180 minutes',
-);
-
-$new_movie_2 = new Movie(
-    'Spider-Man',
-    $new_genre_2,
-    'US / CAN',
-    '120 minutes',
-);
-var_dump($new_movie->getFullInfo());
-var_dump($new_movie->genre->getFullGenre());
-
-echo '<hr>';
-
-var_dump($new_movie_2->getFullInfo());
-var_dump($new_movie_2->genre->getFullGenre());
 
 ?>
 
@@ -49,6 +13,13 @@ var_dump($new_movie_2->genre->getFullGenre());
     <title>PHP Movies</title>
 </head>
 <body>
-    
+    <h1>Film</h1>
+    <ul>
+        <?php foreach($movie as $movie): ?>
+        <li>
+            <?= $movie->getFullInfo() ?> - <?= $movie->genre->getFullGenre() ?>
+        </li>
+        <?php endforeach; ?>
+    </ul>
 </body>
 </html>
